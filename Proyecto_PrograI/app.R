5b70ab7bb28b9b1254a373d5aceba2512462cb4d
Santiago-MunozC <munozcordobasantiago@gmail.com>
2026-06-17 23:51:23 -0600

    Intento de pestaña 1, histogramas o gráficos de barras para cada variable según corresponda. Además, carga del dataset final y limpio

--- Proyecto_PrograI/app.R
+++ Proyecto_PrograI/app.R
@@ -6,46 +6,79 @@
 #
 #    https://shiny.posit.co/
 #
-
 library(shiny)
+library(ggplot2)
 
-# Define UI for application that draws a histogram
+# Define UI for application
 ui <- fluidPage(
+  
+  # Application title
+  titlePanel("Music Explorer: Análisis interactivo de canciones y artistas"),
+  
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
+  tabsetPanel(
+    
+    # a. Descripción y exploración inicial
+    tabPanel("Exploración Inicial",
+             sidebarLayout(
+               
+               sidebarPanel(
+                 selectInput("var_explorar", "Seleccione una variable para visualizar:", 
+                             choices = list(
+                               "Cuantitativas" = c("Danceability" = "Danceability", 
+                                                   "Energy" = "Energy", 
+                                                   "Valence" = "Valence",
+                                                   "Instrumentalness" = "Instrumentalness",
+                                                   "Duration_min" = "Duration_min",
+                                                   "Liveness" = "Liveness"),
+                               
+                               "Cualitativas" = c("Género Musical" = "Genre",
+                                                  "Tipo de Álbum" = "Album_type",
+                                                  "Plataforma" = "most_playedon",
+                                                  "Artista" = "Artist") 
+                             ) 
+                 ) 
+               ), 
+               
+               mainPanel(
+                 plotOutput("hist_exploracion"),
+                 p("Esta pestaña muestra la distribución general de las características musicales.")
+               )
+               
+             ) 
+    ) 
+  ) 
+) 
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
 
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
-)
-
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+# 3. Lógica del Servidor
+server <- function(input, output){
+  
+  output$hist_exploracion <- renderPlot({
+    columna_seleccionada <- dataset[[input$var_explorar]]
+    if(is.numeric(columna_seleccionada)) {
+      
+      # Si es numérica, hacemos el HISTOGRAMA
+      ggplot(dataset, aes_string(x = input$var_explorar)) +
+        geom_histogram(fill = "steelblue", color = "white", bins = 20) +
+        theme_minimal() +
+        labs(title = paste("Distribución de", input$var_explorar), 
+             y = "Frecuencia", 
+             x = input$var_explorar)
+      
+    } else {
+      
+      # Si es categórica/texto, hacemos el GRÁFICO DE BARRAS HORIZONTALES
+      ggplot(dataset, aes_string(x = input$var_explorar)) +
+        geom_bar(fill = "coral", color = "black") +
+        coord_flip() + 
+        theme_minimal() +
+        labs(title = paste("Frecuencia de", input$var_explorar), 
+             y = "Cantidad de Canciones", 
+             x = input$var_explorar)
+    }
+  })
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
